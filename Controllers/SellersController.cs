5fe079a095a89c7e806fdd317e181bf55178c589
Gustavo <gustavoskt.8@hotmail.com>
2024-08-19 18:41:50 -0300

    criação de um projeto de estoque, primeiras classes e migration com sucesso

---
+++ Controllers/SellersController.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProjetoEstoqueMVC.Models;
+using ProjetoEstoqueMVC.Services;
+
+namespace ProjetoEstoqueMVC.Controllers
+{
+    public class SellersController : Controller
+    {
+        private readonly SellerService _sellerService;
+
+        public SellersController(SellerService sellerService)
+        {
+            _sellerService = sellerService;
+        }
+        public IActionResult Index()
+        {
+            var list = _sellerService.FindAll();
+            return View(list);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("Create")]
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
+        }
+
+    }
+}
---
+++ Services/SellerService.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ProjetoEstoqueMVC.Context;
+using ProjetoEstoqueMVC.Models;
+
+namespace ProjetoEstoqueMVC.Services
+{
+    public class SellerService
+    {
+      private readonly AppDbContext _context;
+
+        public SellerService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<Seller> FindAll()
+        {
+            return _context.Seller.ToList();
+        }
+
+        public void Insert(Seller obj)
+        {
+            _context.Add(obj);
+            _context.SaveChanges();
+        }
+
+    }
+}
